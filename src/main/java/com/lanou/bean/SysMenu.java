25be7a6bf0e119d2cc6b7139101a188f722c6c98
dllo <muyongrui001@163.com>
2017-11-14 15:23:51 +0800

    接着上次

---
+++ src/main/java/com/lanou/bean/SysMenu.java
@@ -0,0 +1,191 @@
+package com.lanou.bean;
+
+import java.util.Date;
+import java.util.List;
+
+public class SysMenu {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String icon;
+
+    private String name;
+
+    private String urlkey;
+
+    private String url;
+
+    private Integer status;
+
+    private Integer type;
+
+    private Integer sort;
+
+    private Integer level;
+
+    private Date createTime;
+
+    private Integer createId;
+
+    private Date updateTime;
+
+    private Integer updateId;
+
+    private String upName;
+
+
+  private User user;
+
+    public String getUpName() {
+        return upName;
+    }
+
+    public void setUpName(String upName) {
+        this.upName = upName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getUrlkey() {
+        return urlkey;
+    }
+
+    public void setUrlkey(String urlkey) {
+        this.urlkey = urlkey == null ? null : urlkey.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+
+    @Override
+    public String toString() {
+        return "SysMenu{" +
+                "id=" + id +
+                ", parentId=" + parentId +
+                ", icon='" + icon + '\'' +
+                ", name='" + name + '\'' +
+                ", urlkey='" + urlkey + '\'' +
+                ", url='" + url + '\'' +
+                ", status=" + status +
+                ", type=" + type +
+                ", sort=" + sort +
+                ", level=" + level +
+                ", createTime=" + createTime +
+                ", createId=" + createId +
+                ", updateTime=" + updateTime +
+                ", updateId=" + updateId +
+                ", upName='" + upName + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
